eb774318e89ec06f66c6eaf572a6627726762630
ProfKambing <fawwaziqbal2008@gmail.com>
2021-09-05 21:45:29 +0800

    fixed alphastep hud.

--- Client-2.0.0-github/Client-2.0.0/src/main/java/client/modules/client/Hud.java
+++ Client-2.0.0-github/Client-2.0.0/src/main/java/client/modules/client/Hud.java
@@ -108,7 +108,7 @@ public class Hud extends Module {
                 for (int k = 0; k < Client.moduleManager.sortedModules.size(); k++) {
                     Module module = Client.moduleManager.sortedModules.get(k);
                     String str = module.getDisplayName() + ((module.hudInfoString() != null) ? (ChatFormatting.WHITE + " [" + module.hudInfoString() + "]") : "");
-                    renderer.drawString(str, (width - 2 - renderer.getStringWidth(str)), (2 + j * 10), ColorUtil.alphaStep(new Color(color), index.getCurrentState(), (count + countt.getCurrentState())).getRGB(), true);
+                    renderer.drawString(str, (width - 2 - renderer.getStringWidth(str)), (2 + j * 10), ColorUtil.alphaStep(new Color(color), index.getCurrentState(), (counter1[0] + countt.getCurrentState())).getRGB(), true);
                     j++;
                     counter1[0] = counter1[0] + 1;
                     count++;
