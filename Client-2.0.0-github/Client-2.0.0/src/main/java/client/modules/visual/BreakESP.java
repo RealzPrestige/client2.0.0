281a6ea641638646b8222e00878e4578ff455487
vikkn <vikaslagerweij@gmail.com>
2021-09-05 21:37:35 +0200

    woopsies

--- Client-2.0.0-github/Client-2.0.0/src/main/java/client/modules/visual/BreakESP.java
+++ Client-2.0.0-github/Client-2.0.0/src/main/java/client/modules/visual/BreakESP.java
@@ -11,13 +11,12 @@ import java.util.Objects;
 
 public class BreakESP extends Module {
     public Setting<Boolean> box = register(new Setting<>("Box", true));
-    public Setting<Boolean> outline = register(new Setting<>("Box", true));
+    public Setting<Boolean> outline = register(new Setting<>("Outline", true));
     public Setting<Integer> red = register(new Setting<>("Red", 0, 0, 255));
     public Setting<Integer> green = register(new Setting<>("Green", 255, 0, 255));
     public Setting<Integer> blue = register(new Setting<>("Blue", 0, 0, 255));
     public Setting<Integer> alpha = register(new Setting<>("Alpha", 120, 0, 255));
 
-    Color color = new Color(red.getCurrentState(), green.getCurrentState(), blue.getCurrentState(), alpha.getCurrentState());
     String string;
     public BreakESP(){
         super("BreakESP", "Shows where spots are being broken.", Category.VISUAL);
@@ -28,7 +27,6 @@ public class BreakESP extends Module {
         mc.renderGlobal.damagedBlocks.forEach(((integer, destroyBlockProgress) -> {
             RenderUtil.drawBoxESP(destroyBlockProgress.getPosition(), new Color(red.getCurrentState(), green.getCurrentState(), blue.getCurrentState(), alpha.getCurrentState()), true, new Color(red.getCurrentState(), green.getCurrentState(), blue.getCurrentState(), alpha.getCurrentState()),1, outline.getCurrentState(), box.getCurrentState(), alpha.getCurrentState(), true);
             RenderUtil.drawText(destroyBlockProgress.getPosition(), Objects.requireNonNull(mc.world.getEntityByID(integer)).getName() + " " + string + (destroyBlockProgress.getPartialBlockDamage() * 12.5) + "%");
-
         }));
     }
 
