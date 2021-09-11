534e7748424d74b9c1ee1c0174be33f8fed889c9
vikkn <vikaslagerweij@gmail.com>
2021-09-11 04:11:39 +0200

    nametag fix 1/2

--- src/main/java/client/modules/visual/NameTags.java
+++ src/main/java/client/modules/visual/NameTags.java
@@ -44,9 +44,12 @@ public class NameTags extends Module {
     public void onRender3D(Render3DEvent event) {
         if (!NameTags.fullNullCheck()) {
             for (EntityPlayer players : mc.world.playerEntities) {
-                if (players.getName().equals(mc.player.getName()) || !players.isDead || players.getDistance(mc.player) < 300 || !EntityUtil.isInFov(players) || !players.isDead || players.getEntityId() != 6900 || !players.getName().equals("Pop")) {
+                if (!players.getName().equals(mc.player.getName()) && !players.isDead && EntityUtil.isInFov(players) && !players.isDead && players.getEntityId() != 6900 && !players.getName().equals("Pop")) {
                     entities.add(players);
                 } else {
+                    entities.remove(players);
+                }
+                if(players == null){
                     entities.remove(players);
                 }
 
