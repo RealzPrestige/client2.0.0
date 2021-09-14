82772f21fb77ed5057aca9b5872e61117a26cc88
ProfKambing <fawwaziqbal2008@gmail.com>
2021-09-14 11:31:05 +0800

    some popchams improvements

--- src/main/java/client/modules/visual/PopChamsRewrite.java
+++ src/main/java/client/modules/visual/PopChamsRewrite.java
@@ -1,8 +1,6 @@
 package client.modules.visual;
 
-import client.Client;
 import client.events.Render3DEvent;
-import client.events.TotemPopEvent;
 import client.gui.impl.setting.Setting;
 import client.modules.Module;
 import client.util.RenderUtil;
@@ -12,7 +10,6 @@ import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
 
 import static client.util.RenderUtil.renderEntity;
@@ -32,6 +29,8 @@ public class PopChamsRewrite extends Module {
      */
 
     public Setting<Boolean> self = register(new Setting<>("Self", true));
+    public Setting<Integer> range = register(new Setting<>("Range", 30, 10, 100));
+    public Setting<Boolean> outline = register(new Setting<>("Outline", true));
     public Setting<Integer> fadeStart = register(new Setting<>("FadeTime", 1000, 0, 5000));
     public Setting<Integer> red = register(new Setting<>("Red", 255, 0, 255));
     public Setting<Integer> green = register(new Setting<>("Green", 255, 0, 255));
@@ -43,12 +42,25 @@ public class PopChamsRewrite extends Module {
 
         public void k(EntityPlayer poppedPlayer) {
             if (self.getCurrentState() || poppedPlayer != mc.player) {
+                if (mc.player.getDistance(player) < range.getValue()){
                 GameProfile profile = new GameProfile(mc.player.getUniqueID(), "");
                 player = new EntityOtherPlayerMP(mc.world, profile);
                 player.copyLocationAndAnglesFrom(poppedPlayer);
                 startTime = System.currentTimeMillis();
             }
+            }
         }
+        @Override
+        public void onUpdate(){
+        for (EntityPlayer entity : mc.world.playerEntities) {
+            if (entity.getName().equals("FakePlayer")) {
+                k(entity);
+            }
+
+        }
+
+        }
+
         @Override
         public void onRender3D(Render3DEvent event) {
             playerModel = new ModelPlayer(0, false);
@@ -56,12 +68,14 @@ public class PopChamsRewrite extends Module {
             GL11.glLineWidth(1);
 
             int fillA = alpha.getCurrentState();
+            int lineA = 255;
 
             if (System.currentTimeMillis() - startTime > fadeStart.getCurrentState()) {
                 long time = System.currentTimeMillis() - startTime - fadeStart.getCurrentState();
                 double normal = normalize(((double) time), 0, fadeStart.getCurrentState());
                 normal = MathHelper.clamp(normal, 0, 1);
                 normal = (-normal) + 1;
+                lineA = (int) (normal * lineA);
                 fillA = (int) (normal * fillA);
             }
             if (player != null) {
@@ -83,6 +97,22 @@ public class PopChamsRewrite extends Module {
                 glEnable(2896);
                 GlStateManager.popAttrib();
                 GlStateManager.popMatrix();
+                if (outline.getValue()) {
+                    GlStateManager.pushMatrix();
+                    GL11.glPushAttrib(1048575);
+                    GL11.glPolygonMode(1032, 6913);
+                    glDisable(3553);
+                    glDisable(2896);
+                    glDisable(2929);
+                    glEnable(2848);
+                    glEnable(3042);
+                    GL11.glBlendFunc(770, 771);
+                    GL11.glColor4f(red.getCurrentState()/255.0f, green.getCurrentState()/255.0f, blue.getCurrentState()/255.0f, lineA/255.0f);
+                    renderEntity(player, playerModel, 0, 0, player.ticksExisted, player.rotationYawHead, player.rotationPitch, 1);
+                    glEnable(2896);
+                    GlStateManager.popAttrib();
+                    GlStateManager.popMatrix();
+                }
                 RenderUtil.RenderTesselator.releaseGL();
             }
         }
