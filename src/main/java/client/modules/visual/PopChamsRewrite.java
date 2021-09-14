f93528c596fb844bc9ef6d1d66ad347d892cdb89
ProfKambing <fawwaziqbal2008@gmail.com>
2021-09-14 11:32:44 +0800

    onemore

--- src/main/java/client/modules/visual/PopChamsRewrite.java
+++ src/main/java/client/modules/visual/PopChamsRewrite.java
@@ -30,6 +30,7 @@ public class PopChamsRewrite extends Module {
 
     public Setting<Boolean> self = register(new Setting<>("Self", true));
     public Setting<Integer> range = register(new Setting<>("Range", 30, 10, 100));
+    public Setting<Boolean> solid = register(new Setting<>("Solid", true));
     public Setting<Boolean> outline = register(new Setting<>("Outline", true));
     public Setting<Integer> fadeStart = register(new Setting<>("FadeTime", 1000, 0, 5000));
     public Setting<Integer> red = register(new Setting<>("Red", 255, 0, 255));
@@ -80,23 +81,25 @@ public class PopChamsRewrite extends Module {
             }
             if (player != null) {
                 RenderUtil.RenderTesselator.prepareGL();
-                GlStateManager.pushMatrix();
-                GL11.glPushAttrib(1048575);
-                glDisable(3553);
-                glDisable(2896);
-                glEnable(2848);
-                glEnable(3042);
-                GL11.glBlendFunc(770, 771);
-                glDisable(2929);
-                GL11.glDepthMask(false);
-                GL11.glColor4f(red.getCurrentState()/255.0f, green.getCurrentState()/255.0f, blue.getCurrentState()/255.0f, fillA/255.0f);
-                renderEntity(player, playerModel, 0, 0, player.ticksExisted, player.rotationYawHead, player.rotationPitch, 1);
-                glDisable(2896);
-                glEnable(2929);
-                renderEntity(player, playerModel, 0, 0, player.ticksExisted, player.rotationYawHead, player.rotationPitch, 1);
-                glEnable(2896);
-                GlStateManager.popAttrib();
-                GlStateManager.popMatrix();
+                if (solid.getValue()) {
+                    GlStateManager.pushMatrix();
+                    GL11.glPushAttrib(1048575);
+                    glDisable(3553);
+                    glDisable(2896);
+                    glEnable(2848);
+                    glEnable(3042);
+                    GL11.glBlendFunc(770, 771);
+                    glDisable(2929);
+                    GL11.glDepthMask(false);
+                    GL11.glColor4f(red.getCurrentState() / 255.0f, green.getCurrentState() / 255.0f, blue.getCurrentState() / 255.0f, fillA / 255.0f);
+                    renderEntity(player, playerModel, 0, 0, player.ticksExisted, player.rotationYawHead, player.rotationPitch, 1);
+                    glDisable(2896);
+                    glEnable(2929);
+                    renderEntity(player, playerModel, 0, 0, player.ticksExisted, player.rotationYawHead, player.rotationPitch, 1);
+                    glEnable(2896);
+                    GlStateManager.popAttrib();
+                    GlStateManager.popMatrix();
+                }
                 if (outline.getValue()) {
                     GlStateManager.pushMatrix();
                     GL11.glPushAttrib(1048575);
